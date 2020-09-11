c0cb2071545f1ad9eb7c792b609431b0c0b2aeba
Roy Gutierrez <roymg29@gmail.com>
2020-09-11 16:22:45 -0400

    contact information to ask keah

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const contact = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Keah Gutierrez</h1>
+      <p>Email: *email</p>
+      <p>Phone:??</p>
+      <p>Resume: ??</p>
+    </div>
+  );
 };
 
 export default contact;
