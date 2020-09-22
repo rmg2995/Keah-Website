14609deb6a4f9504d9de99ab618c5714faa971bd
Roy Gutierrez <roymg29@gmail.com>
2020-09-22 16:20:53 -0400

    started-css-on-navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from "react";
 // import { Link } from "react-router-dom";
 import { HashLink as Link } from "react-router-hash-link";
+import "../styles/navbar.scss";
 const Navbar = () => {
   return (
-    <div>
-      <h1>This is the Navbar</h1>
+    <div className="navbar">
       <Link to="/about#yourAnchorTag">About Me</Link>
       <Link to="/#top">Home</Link>
       {/* <Link to="/about">About Me</Link> */}
