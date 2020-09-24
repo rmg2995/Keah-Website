d08d650875bd964fca99891898e0ca8098515ff1
Roy Gutierrez <roymg29@gmail.com>
2020-09-24 11:04:05 -0400

    styled-and-added-pic-for-two-sections

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,22 +3,28 @@ import Keah from "./images/Gutierrez_Keah copy.jpg";
 import "../styles/aboutme.scss";
 const AboutMe = () => {
   return (
-    <div id="yourAnchorTag">
-      <h2>Profile</h2>
-      <p>I'm a future Pharmacist</p>
-      <h3>About Me</h3>
-      <p>For Keah to write</p>
+    <div id="yourAnchorTag" className="about-me">
+      <section>
+        <h2>Profile</h2>
+        <p>I'm a future Pharmacist</p>
+      </section>
+      <section>
+        <h3>About Me</h3>
+        <p>For Keah to write</p>
+      </section>
       <img src={Keah} alt="Keah professional photo" />
-      <h3>Details</h3>
-      <p>
-        <strong>Name:</strong> Keah Gutierrez
-      </p>
-      <p>
-        <strong>Age:</strong> 25
-      </p>
-      <p>
-        <strong>Location:</strong> Miami, Fl, USA
-      </p>
+      <section>
+        <h3>Details</h3>
+        <p>
+          <strong>Name:</strong> Keah Gutierrez
+        </p>
+        <p>
+          <strong>Age:</strong> 25
+        </p>
+        <p>
+          <strong>Location:</strong> Miami, Fl, USA
+        </p>
+      </section>
     </div>
   );
 };
